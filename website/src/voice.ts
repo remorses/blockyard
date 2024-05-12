ed9e10cb5f8ea067015ea166474564300e390a4b
remorses <beats.by.morse@gmail.com>
2024-05-12 15:28:19 +0200

    website/src/voice: nn

--- website/src/voice.ts
+++ website/src/voice.ts
@@ -7,71 +7,75 @@ const peersRef: Record<string, Peer> = {}
 const audioElementsRef = {}
 let hostPeerId = 'minecord-host'
 export async function voiceChat() {
-    const { peers } = voxelizeState
+    try {
+        const { peers } = voxelizeState
 
-    const clients = peers.children
-    const clientId = peers.ownID.replace(/[-_]/g, '')
+        const clients = peers.children
+        const clientId = peers.ownID.replace(/[-_]/g, '')
 
-    const isInitializer = window.location.hash === '#init'
-    const peer = new Peer(isInitializer ? hostPeerId : clientId, {})
+        const isInitializer = window.location.hash === '#init'
+        const peer = new Peer(isInitializer ? hostPeerId : clientId, {})
 
-    const conn = peer.connect(hostPeerId)
+        const conn = peer.connect(hostPeerId)
 
-    const { events } = voxelizeState
+        const { events } = voxelizeState
 
-    peer.on('open', (id) => {
-        console.log('My peer ID is: ' + id)
-    })
-    peer.on('error', (error) => {
-        console.error('[peerjs error]', error)
-    })
-
-    // Handle incoming data connection
-    peer.on('connection', (conn) => {
-        console.log('incoming peer connection!')
-        conn.on('data', (data) => {
-            console.log(`received: ${data}`)
+        peer.on('open', (id) => {
+            console.log('My peer ID is: ' + id)
         })
-        conn.on('open', () => {
-            conn.send('hello!')
+        peer.on('error', (error) => {
+            console.error('[peerjs error]', error)
         })
-    })
-    const onStream = (mediaStream) => {
-        const audio = new Audio()
-        audio.autoplay = true
-        // audio.muted = remoteAudioMuted[targetId]
-        audio.srcObject = mediaStream
-        audio.play()
 
-        // audioElementsRef[call.peer] = audio
-    }
-    if (!isInitializer) {
-        const stream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-        })
-        const call = peer.call(hostPeerId, stream)
-        call.on('stream', onStream)
-    }
-
-    peer.on('call', (call) => {
-        navigator.mediaDevices
-            .getUserMedia({ video: true, audio: true })
-            .then((stream) => {
-                call.answer(stream) // Answer the call with an A/V stream.
-                call.on('stream', onStream)
+        // Handle incoming data connection
+        peer.on('connection', (conn) => {
+            console.log('incoming peer connection!')
+            conn.on('data', (data) => {
+                console.log(`received: ${data}`)
             })
-            .catch((err) => {
-                console.error('Failed to get local stream', err)
+            conn.on('open', () => {
+                conn.send('hello!')
             })
-    })
-    peer.on('close', () => {
-        const targetId = peer.id
-        peer.destroy()
-        delete peersRef[targetId]
-        if (audioElementsRef[targetId]) {
-            audioElementsRef[targetId].pause()
-            audioElementsRef[targetId].srcObject = null
-            delete audioElementsRef[targetId]
+        })
+        const onStream = (mediaStream) => {
+            const audio = new Audio()
+            audio.autoplay = true
+            // audio.muted = remoteAudioMuted[targetId]
+            audio.srcObject = mediaStream
+            audio.play()
+
+            // audioElementsRef[call.peer] = audio
         }
-    })
+        if (!isInitializer) {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+            })
+            const call = peer.call(hostPeerId, stream)
+            call.on('stream', onStream)
+        }
+
+        peer.on('call', (call) => {
+            navigator.mediaDevices
+                .getUserMedia({ video: true, audio: true })
+                .then((stream) => {
+                    call.answer(stream)
+                    call.on('stream', onStream)
+                })
+                .catch((err) => {
+                    console.error('Failed to get local stream', err)
+                })
+        })
+        peer.on('close', () => {
+            const targetId = peer.id
+            peer.destroy()
+            delete peersRef[targetId]
+            if (audioElementsRef[targetId]) {
+                audioElementsRef[targetId].pause()
+                audioElementsRef[targetId].srcObject = null
+                delete audioElementsRef[targetId]
+            }
+        })
+    } catch (error) {
+        console.error('voiceChat error', error)
+    }
 }
