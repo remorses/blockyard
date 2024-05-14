afab386b7b1f10bc463933093de5dd72145f91bc
remorses <beats.by.morse@gmail.com>
2024-05-14 17:49:36 +0200

    enable rls

---
+++ db/scripts/enable-rls.ts
@@ -0,0 +1,22 @@
+import { shell } from '@xmorse/deployment-utils/src'
+import { Prisma, prisma } from '../prisma'
+
+const str = [
+    Prisma.sql`
+    DO $$
+    DECLARE
+        r RECORD;
+    BEGIN
+        FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+            EXECUTE format('ALTER TABLE %I ENABLE ROW LEVEL SECURITY;', r.tablename);
+        END LOOP;
+    END
+    $$;
+    `,
+]
+async function main() {
+    for (let line of str) {
+        await prisma.$executeRaw(line)
+    }
+}
+main()
