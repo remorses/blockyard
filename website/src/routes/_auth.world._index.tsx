962866a7462975b2df446b1ea6cae87abf667371
remorses <beats.by.morse@gmail.com>
2024-05-18 15:13:20 +0200

    website/src/routes/_auth.world._index: return redirect

--- website/src/routes/_auth.world._index.tsx
+++ website/src/routes/_auth.world._index.tsx
@@ -22,14 +22,17 @@ import {
     Window,
 } from 'react95'
 import {
+    RemixFormProvider,
     getValidatedFormData,
     useRemixForm,
+    useRemixFormContext,
     validateFormData,
 } from 'remix-hook-form'
 import { Resolver, zodResolver } from '@hookform/resolvers/zod'
 import { ProgressBarIncrementing, Windows95Provider } from '~/components/95'
 import { sleep, getWorldUrl } from '~/lib/utils'
 import { useNavigation } from '@remix-run/react'
+import { useFormContext } from 'react-hook-form'
 
 export const meta: MetaFunction = () => {
     return [
@@ -40,7 +43,7 @@ export const meta: MetaFunction = () => {
 
 const schema = z.object({
     option: z.enum(['create', 'join']),
-    worldName: z.string().optional(),
+    worldId: z.string().optional(),
 })
 const resolver = zodResolver(schema)
 
@@ -57,39 +60,51 @@ export async function action({ request }: ActionFunctionArgs) {
         request,
         resolver,
     )
-    console.log({ receivedValues, data, errors })
     if (errors) {
         return json({ errors, defaultValues: {} })
     }
-    let [org] = await Promise.all([
-        prisma.org.findFirst({
-            where: { users: { some: { userId } } },
-        }),
-    ])
-    let orgId = org?.orgId as string
-    if (!org) {
-        org = await prisma.org.create({
-            data: {
-                users: {
-                    create: { userId, role: 'ADMIN' },
+
+    if (data.option === 'join') {
+        const found = await prisma.world.findFirst({
+            where: { id: data.worldId },
+        })
+        if (!found) {
+            return json(
+                {
+                    errors: {
+                        worldId: { message: 'World not found' },
+                    },
                 },
+                { status: 400 },
+            )
+        }
+        return redirect(`/world/${data.worldId}`, { headers })
+    } else if (data.option === 'create') {
+        let [org] = await Promise.all([
+            prisma.org.findFirst({
+                where: { users: { some: { userId } } },
+            }),
+        ])
+        let orgId = org?.orgId as string
+        if (!org) {
+            org = await prisma.org.create({
+                data: {
+                    users: {
+                        create: { userId, role: 'ADMIN' },
+                    },
+                },
+            })
+            orgId = org.orgId
+        }
+
+        const world = await prisma.world.create({
+            data: {
+                createdByUserId: userId,
+                orgId,
             },
         })
-        orgId = org.orgId
+        return redirect(`/world/${world.id}`, { headers })
     }
-
-    const world = await prisma.world.create({
-        data: {
-            createdByUserId: userId,
-            orgId,
-        },
-    })
-    return redirect(`/world/${world.id}`, { headers })
-    return json({
-        receivedValues,
-        world,
-        data,
-    })
 }
 
 export let loader = async ({ request, params }: LoaderFunctionArgs) => {
@@ -126,15 +141,17 @@ export let loader = async ({ request, params }: LoaderFunctionArgs) => {
 export default function Page() {
     const { worlds } = useLoaderData<typeof loader>()
     const actionData = useActionData<typeof action>()
+    const methods = useRemixForm<Data>({
+        resolver,
+        // errors: actionData?.errors,
+    })
     const {
         handleSubmit,
         formState: { errors, isSubmitting },
         register,
 
         control,
-    } = useRemixForm<z.infer<typeof schema>>({
-        resolver,
-    })
+    } = methods
 
     const navigation = useNavigation()
 
@@ -149,28 +166,21 @@ export default function Page() {
                             onSubmit={handleSubmit}
                             className='flex-col gap-6 flex'
                         >
-                            <Button
-                                type='submit'
-                                {...register('option', { value: 'create' })}
-                            >
-                                Create World
-                            </Button>
-                            <Separator />
-                            <div className=''>or join an existing world</div>
-                            <div className='flex items-stretch gap-4'>
+                            <RemixFormProvider {...methods}>
                                 <Button
                                     type='submit'
-                                    {...register('option', { value: 'join' })}
+                                    {...register('option', { value: 'create' })}
                                 >
-                                    Join World
+                                    Create World
                                 </Button>
-                                <TextInput
-                                    placeholder='world code'
-                                    variant='flat'
-                                    {...register('worldName', {})}
-                                />
-                            </div>
-                            {isSubmitting && <ProgressBarIncrementing />}
+                                <Separator />
+                                <div className=''>
+                                    or join an existing world
+                                </div>
+                                <JoinAnotherWorld />
+
+                                {isSubmitting && <ProgressBarIncrementing />}
+                            </RemixFormProvider>
                         </Form>
                     </WindowContent>
                 </Window>
@@ -182,7 +192,7 @@ export default function Page() {
         <div className='flex flex-col items-center pt-12'>
             <Window className='w-[700px] mx-auto'>
                 <WindowHeader>Your Worlds</WindowHeader>
-                <WindowContent>
+                <WindowContent className='flex flex-col gap-4'>
                     <div className='flex flex-col gap-4'>
                         {worlds.map((world) => {
                             const link = getWorldUrl({ worldId: world.id })
@@ -202,13 +212,8 @@ export default function Page() {
                                         readOnly
                                     />
                                     {/* <div>{world.createdByUserId}</div> */}
-                                    <Link
-                                        to={`/world/${world.id}`}
-                                    >
+                                    <Link to={`/world/${world.id}`}>
                                         <Button type='button' primary>
-                                            {navigation.state === 'loading' && (
-                                                <Hourglass className='mr-2 w-6' />
-                                            )}
                                             Start Meeting
                                         </Button>
                                     </Link>
@@ -216,8 +221,42 @@ export default function Page() {
                             )
                         })}
                     </div>
+                    <Separator />
+                    <div className=''>Or join another one</div>
+                    <Form
+                        method='POST'
+                        onSubmit={handleSubmit}
+                        className='flex-col gap-6 flex'
+                    >
+                        <RemixFormProvider {...methods}>
+                            <JoinAnotherWorld />
+                        </RemixFormProvider>
+                    </Form>
+                    {isSubmitting && <ProgressBarIncrementing />}
                 </WindowContent>
             </Window>
+        </div>
+    )
+}
+
+function JoinAnotherWorld() {
+    const { formState, register } = useRemixFormContext<Data>()
+    return (
+        <div className='flex items-stretch gap-4'>
+            <Button type='submit' {...register('option', { value: 'join' })}>
+                Join World
+            </Button>
+            <TextInput
+                placeholder='world code'
+                variant='flat'
+                {...register('worldId', {})}
+            />
+            {formState?.errors?.worldId && (
+                <div className='text-red-500'>
+                    {formState?.errors?.worldId?.message}
+                </div>
+            )}
+            {/* {formState.isSubmitting && 'loading...'} */}
         </div>
     )
 }
