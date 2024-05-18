19f0a7bb1688a264729801e168e564e419b58ecc
remorses <beats.by.morse@gmail.com>
2024-05-18 16:45:33 +0200

    cross compiling not feasible because no one has published a toolchain with c++ in it, only gcc and c libs

--- server/scripts/deploy.ts
+++ server/scripts/deploy.ts
@@ -2,18 +2,11 @@ import { shell, deployFly } from '@xmorse/deployment-utils'
 import path from 'path'
 
 async function main() {
-    await Promise.all([
-        shell(`cargo build --release --target=x86_64-unknown-linux-gnu`, {
-            env: {
-                CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER:
-                    'x86_64-linux-gnu-gcc',
-            },
-        }),
-    ])
+    process.chdir('..')
     await deployFly({
         appName: 'minecord-rust-server',
         port: 4000,
-        dockerfile: 'Dockerfile',
+        dockerfile: 'server/Dockerfile',
         minInstances: 1,
         maxInstances: 1,
         memorySize: '1gb',
