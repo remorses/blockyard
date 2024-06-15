cfc09ec067ffef1396dcf0b3171ce0de7822effd
remorses <beats.by.morse@gmail.com>
2024-06-15 18:26:36 +0200

    NN

--- db/supabase.ts
+++ db/supabase.ts
@@ -1,11 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
-import { Database } from './supabase.types'
 
 export function createSupabaseAdmin({
     cacheTags = undefined as string[] | undefined,
     revalidate = undefined as number | undefined,
 } = {}) {
-    return createClient<Database>(
+    return createClient<any>(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.SUPABASE_SERVICE_ROLE_KEY!,
         {
@@ -39,7 +38,7 @@ export function createSupabaseAdmin({
 }
 
 export function createSupabaseAnon() {
-    return createClient<Database>(
+    return createClient<any>(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
         {
